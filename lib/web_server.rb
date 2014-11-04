6b4f979f465329d4b9c88a7ad76171f77b5746e4
Hiroyuki Sano <sh19910711@gmail.com>
2014-11-04 16:39:57 +0900

    wip: web_server: use empty.slim

--- lib/web_server.rb
+++ lib/web_server.rb
@@ -7,7 +7,23 @@ class WebServer < Sinatra::Base
   set :public_folder, Proc.new { File.join(root, "../tmp/web_server") }
 
   get "/" do
-    slim :index
+    slim :empty
+  end
+
+  get "/repositories" do
+    slim :empty
+  end
+
+  get "/repositories/:repo_id" do
+    slim :empty
+  end
+
+  get "/search" do
+    slim :empty
+  end
+
+  get "/search-result" do
+    slim :empty
   end
 
 end
