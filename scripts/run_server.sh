d7c0e5c3376179a681dd12347ec31910d884e46a
Hiroyuki Sano <sh19910711@gmail.com>
2015-01-14 10:58:44 +0900

    run_server.sh: export envs

--- scripts/run_server.sh
+++ scripts/run_server.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 
-SSPM_WEBAPI_HOST=localhost:19292 gulp build
+export SSPM_WEBAPI_HOST=localhost:19292
+export SSPM_ASSETS_HOST=http://localhost:19292
+export SSPM_DEBUG="true"
 
-# http://localhost:19292
-SSPM_ASSETS_HOST="http://localhost:19292" SSPM_DEBUG="true" bundle exec rackup --host localhost --port 19292
+gulp build
+bundle exec rackup --host localhost --port 19292
