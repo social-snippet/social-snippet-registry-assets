51957d2d2f40d2854784c4d0063417c25b7c7fb8
Hiroyuki Sano <sh19910711@gmail.com>
2015-01-14 11:33:36 +0900

    run_server: stop build if webpack-dev-server is running

--- scripts/run_server.sh
+++ scripts/run_server.sh
@@ -3,6 +3,7 @@
 export SSPM_WEBAPI_HOST=localhost:19292
 export SSPM_ASSETS_HOST=http://localhost:19292
 export SSPM_DEBUG="true"
+export SSPM_LOCAL_STORAGE="true"
 
-gulp build
+[ $SSPM_WEBPACK_DEV_SERVER == "" ] && gulp build
 bundle exec rackup --host localhost --port 19292
